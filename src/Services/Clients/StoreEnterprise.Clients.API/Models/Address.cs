d9e32eb69eb313d3fab8dec859b087ae33367dbb
Pedro Humberto <78572029+PedroHumberto@users.noreply.github.com>
2023-07-03 21:13:46 -0300

    clientsAPI/Models_Created-Client-Addres/-/DomainObjects-CPF-Email-DomainException

---
+++ src/Services/Clients/StoreEnterprise.Clients.API/Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using Core.APP.DomainObjects;
+
+namespace StoreEnterprise.Clients.API.Models
+{
+    public class Address : Entity
+    {
+        public Address(string streetAddress, string number, string unit, string neighborhood, string postalCode, string city, string state)
+        {
+            StreetAddress = streetAddress;
+            Number = number;
+            Unit = unit;
+            Neighborhood = neighborhood;
+            PostalCode = postalCode;
+            City = city;
+            State = state;
+        }
+
+        public string StreetAddress { get; private set; }
+        public string Number { get; private set; }
+        public string Unit { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string PostalCode { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public Guid ClientId { get; private set; }
+        public Client Cliente { get; private set; }
+
+    }
+}
---
+++ src/Services/Clients/StoreEnterprise.Clients.API/Models/Client.cs
@@ -0,0 +1,39 @@
+﻿using Core.APP.Data;
+using Core.APP.DomainObjects;
+
+namespace StoreEnterprise.Clients.API.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public Client(Guid id, string name, string email, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
+            Deleted = false;
+        }
+
+        //EF Relation
+        protected Client() { }
+        //------
+        public string Name { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
+        public bool Deleted { get; private set; }
+        public Address Address { get; private set; }
+
+
+        public void ChangeEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void SetAddress(Address address)
+        {
+            Address = address;
+        }
+
+        
+    }
+}
